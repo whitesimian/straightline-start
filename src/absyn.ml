a12f2357c73442c9a66e66985f5e6619314517bc
Felipe César L. Machado <38870713+whitesimian@users.noreply.github.com>
2020-03-16 19:56:38 -0200

    TP 1

--- src/absyn.ml
+++ src/absyn.ml
@@ -7,8 +7,11 @@ type binop = Plus | Minus | Times | Div
 type stm = CompoundStm of stm * stm
          | AssignStm of id * exp
          | PrintStm of exp list
+         | ReadStm of id
+         | CondStm of exp * stm * stm
+         | WhileStm of exp * stm
 
- and exp = IdExp of id
-         | NumExp of int
-         | OpExp of exp * binop * exp
-         | EseqExp of stm * exp
+and exp = IdExp of id
+        | NumExp of int
+        | OpExp of exp * binop * exp
+        | EseqExp of stm * exp 
